21d245ef348d60dca4c7518dcdab42e78fdd7a67
Zpidero <lluc@vivet.cat>
2024-11-28 22:17:18 +0100

    Update HeapSort.java

--- src/main/java/cat/udl/eps/ed/HeapSort/HeapSort.java
+++ src/main/java/cat/udl/eps/ed/HeapSort/HeapSort.java
@@ -88,10 +88,5 @@ public class HeapSort {
             if(index > heapSize) throw new IndexOutOfBoundsException("This node doesn't exist");
         }
     }
-
-    public static void main(String[] args) {
-        Integer[] a = new Integer[]{3,5,6,7,1,2};
-        HeapSort.sort(a);
-        System.out.println(Arrays.toString(a));
-    }
+    
 }
